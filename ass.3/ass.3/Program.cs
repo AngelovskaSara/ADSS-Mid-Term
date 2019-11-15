2173b6e763209444ceee2e10fb82d5625d7b064e
unknown <angelovskasara15@gmail.com>
2019-11-15 20:26:15 +0100

    Exam

---
+++ ass.3/ass.3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MidTerm_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+            bool x = true;
+
+
+
+            while (x)
+            {
+                try
+                {
+                    Console.WriteLine("Input words: ");
+                    word = Console.ReadLine();
+                    if (FindShort(word) == 0)
+                    {
+                        Console.WriteLine("string is empty");
+
+                    }
+                    else
+                        Console.WriteLine("The lenght of the shortest word is: " + FindShort(word));
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine("incorect data");
+                    //continue; 
+                }
+
+
+            }
+        }
+        public static int FindShort(string s)
+        {
+            var words = s.Split(' ');
+            List<int> wordLengths = new List<int>();
+
+            foreach (var word in words)
+                wordLengths.Add(word.Length);
+
+            wordLengths.Sort();
+
+            return wordLengths[0];
+        }
+
+    }
+}
